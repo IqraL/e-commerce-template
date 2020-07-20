48c5213a9312aa34eb10ab12ebc37988785541ae
IqraL <IqraLatif159@gmail.com>
2020-07-20 12:25:28 +0100

    inital product details page

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Button, Icon, Label } from 'semantic-ui-react'
+
 const productDetailsWrapper = {
     display: 'grid',
     'grid-template-columns': '1fr 1fr',
@@ -6,19 +8,35 @@ const productDetailsWrapper = {
 }
 const productDetailsImgWrapper = {
     display: 'grid',
-    // border: '1px solid black',
-    // width: '250px',
-    // height: '100px',
     'justify-items': 'center',
 }
 const productDetailsImg = {
     width: '100px',
     height: '100px',
 }
-const productDetailsDetailsContainer = {
-    // border: ' 1px blue solid',
-    // width: '100px',
-    // height: '30px',
+const productDetailsDetailsContainer = {}
+const productDetailsHeader = {
+    display: 'grid',
+    'grid-template-columns': '1fr 1fr',
+    'grid-column-gap': '12px',
+}
+
+const productName = { 'font-size': '10px' }
+
+const addToCartButtonUI = {
+    width: '50px',
+    height: '15px',
+    'font-size': '6px',
+    'background-color': 'black',
+    border: 'none',
+    color: 'white',
+}
+
+const productDesUI = {
+    display: 'grid',
+    'justify-items': 'center',
+    'padding-top': '15px',
+    'font-size': '8px',
 }
 
 function ProductDetails() {
@@ -30,7 +48,23 @@ function ProductDetails() {
                     src="https://freemans.scene7.com/is/image/OttoUK/466w/creation-l-warp-look-sweater~E35415FRSC.jpg"
                 />
             </div>
-            <div style={productDetailsDetailsContainer}></div>
+            <div style={productDetailsDetailsContainer}>
+                <div style={productDetailsHeader}>
+                    <div style={productName}>product Name</div>
+                    <Button style={addToCartButtonUI}>Add to Cart</Button>
+                </div>
+                <div style={productDesUI}>
+                    With your permission we and our partners may use precise
+                    geolocation data and identification through device scanning.
+                    You may click to consent to our and our partners’ processing
+                    as described above. Alternatively you may click to refuse to
+                    consent or access more detailed information and change your
+                    preferences before consenting. Please note that some
+                    processing of your personal data may not require your
+                    consent, but you have a right to object to such processing.
+                    Your preferences will apply to this website only.
+                </div>
+            </div>
         </div>
     )
 }
--- src/ProductOverview.js
+++ src/ProductOverview.js
@@ -1,26 +1,37 @@
-import React from "react";
-import "./homePage.css";
+import React from 'react'
+import './homePage.css'
 
 const productOverviewImg = {
-  border: "1px black solid",
-  width: "100px",
-  height: "100px",
-};
+    border: '1px black solid',
+    width: '100px',
+    height: '100px',
+    display: 'grid',
+    'justify-items': 'center',
+}
 const productOverviewDeatils = {
-  border: " 1px black solid",
-  width: "100px",
-  height: "30px",
-};
+    border: ' 1px black solid',
+    width: '100px',
+    height: '30px',
+}
+const productImg = {
+    width: '90px',
+    height: '90px',
+}
 
 function ProductOverview(props) {
-  let { img, name, price } = props;
-  return (
-    <div className="productOverviewWrapper">
-      <div style={productOverviewImg}>{img}</div>
-      <div style={productOverviewDeatils}>
-        {name} £{price}
-      </div>
-    </div>
-  );
+    let { img, name, price } = props
+    return (
+        <div className="productOverviewWrapper">
+            <div style={productOverviewImg}>
+                <img
+                    style={productImg}
+                    src="https://freemans.scene7.com/is/image/OttoUK/466w/creation-l-warp-look-sweater~E35415FRSC.jpg"
+                />
+            </div>
+            <div style={productOverviewDeatils}>
+                {name} £{price}
+            </div>
+        </div>
+    )
 }
-export default ProductOverview;
+export default ProductOverview
