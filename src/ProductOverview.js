30ca59642ba967d0ad4443780f4b88bb07349ab0
IqraL <IqraLatif159@gmail.com>
2020-07-20 12:40:57 +0100

    added product name tester

--- src/ProductOverview.js
+++ src/ProductOverview.js
@@ -1,26 +1,24 @@
-import React from "react";
-import "./homePage.css";
+import React from 'react'
+import './homePage.css'
 
 const productOverviewImg = {
-  border: "1px black solid",
-  width: "100px",
-  height: "100px",
-};
+    border: '1px black solid',
+    width: '100px',
+    height: '100px',
+}
 const productOverviewDeatils = {
-  border: " 1px black solid",
-  width: "100px",
-  height: "30px",
-};
+    border: ' 1px black solid',
+    width: '100px',
+    height: '30px',
+}
 
 function ProductOverview(props) {
-  let { img, name, price } = props;
-  return (
-    <div className="productOverviewWrapper">
-      <div style={productOverviewImg}>{img}</div>
-      <div style={productOverviewDeatils}>
-        {name} £{price}
-      </div>
-    </div>
-  );
+    let { img, name, price } = props
+    return (
+        <div className="productOverviewWrapper">
+            <div style={productOverviewImg}>{img}</div>
+            <div style={productOverviewDeatils}>product name £5.00</div>
+        </div>
+    )
 }
-export default ProductOverview;
+export default ProductOverview
